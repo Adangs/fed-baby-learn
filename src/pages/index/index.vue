25ec3deb1676dd6c7b0bac1e1c71ab3681c8e7af
Adang <lundang.liu@feima-inc.com>
2020-01-18 17:34:45 +0800

    界面开发

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,8 @@
 <template>
   <view class="m-list">
+    <view class="logo">
+      <x-image src="/static/images/144x144.png" />
+    </view>
     <view class="li">
       <view class="item" @click="onNavigateTo('one')">
         <text>单字</text>
@@ -14,9 +17,12 @@
 </template>
 
 <script>
+  import XImage from '@/components/x-image'
   export default {
     name: 'Home',
-    components: {},
+    components: {
+      XImage
+    },
     props: {},
     data() {
       return {};
@@ -40,9 +46,11 @@
   .m-list{
     //  flex-direction: column;
     position: absolute; left: 50%; top: 50%; transform: translate3d(-50%,-50%,0); display: flex;
+    .logo{ position: absolute; width: 144px; height: 144px; left: 50%; top: -200px; transform: translate3d(-50%, 0, 0);}
     .li{ flex: 1; padding: 30px;}
     .item{
       width: 220px; height: 220px; background: #fafafa; border-radius: 20px; border: 2px solid #eee; text-align: center; display: flex; align-items: center;
+      &:active{ opacity: .8;}
       text{ display: block; flex: 1; font-size: 40px;}
     }
   }
