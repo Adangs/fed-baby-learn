ccd3cbf7954a0f97c39c8b64363625e5f60ea7a2
Adang <lundang.liu@feima-inc.com>
2020-01-18 17:36:05 +0800

    界面开发

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -49,9 +49,9 @@
     .logo{ position: absolute; width: 144px; height: 144px; left: 50%; top: -200px; transform: translate3d(-50%, 0, 0);}
     .li{ flex: 1; padding: 30px;}
     .item{
-      width: 220px; height: 220px; background: #fafafa; border-radius: 20px; border: 2px solid #eee; text-align: center; display: flex; align-items: center;
+      width: 220px; height: 220px; background: #fafafa; border-radius: 20px; border: 2px solid #ddd; text-align: center; display: flex; align-items: center;
       &:active{ opacity: .8;}
-      text{ display: block; flex: 1; font-size: 40px;}
+      text{ display: block; flex: 1; font-size: 50px;}
     }
   }
 </style>
