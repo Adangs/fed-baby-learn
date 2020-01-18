b528f7df7db5d614e58e291809f4a1d26bf5d63e
Adang <lundang.liu@feima-inc.com>
2020-01-18 11:35:01 +0800

    界面开发

---
+++ src/pages/details/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <view class="m-details">
+    <view class="word">永</view>
+    <view class="tools">
+
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    name: 'Details',
+    components: {},
+    props: {},
+    data() {
+      return {};
+    },
+    computed: {},
+    watch: {},
+    onLoad() {
+
+    },
+    methods: {}
+  };
+</script>
+
+<style lang="scss">
+  .m-details{
+    position: absolute; width: 100%; height: 100%; left: 0; top: 0;
+  }
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,12 +2,12 @@
   <view class="m-list">
     <view class="li">
       <view class="item">
-        <text>第一阶段</text>
+        <text>单字</text>
       </view>
     </view>
     <view class="li">
       <view class="item">
-        <text>第二阶段</text>
+        <text>词语</text>
       </view>
     </view>
   </view>
@@ -32,11 +32,11 @@
 
 <style lang="scss">
   .m-list{
-    position: absolute; left: 50%; top: 50%; transform: translate3d(-50%,-50%,0); display: flex;
-    .li{ flex: 1; padding: 20px;}
+    position: absolute; left: 50%; top: 50%; transform: translate3d(-50%,-50%,0); display: flex; flex-direction: column;
+    .li{ flex: 1; padding: 30px;}
     .item{
-      width: 200px; height: 200px; background: #fafafa; border-radius: 20px; border: 2px solid #eee; text-align: center; display: flex; align-items: center;
-      text{ display: block; flex: 1;}
+      width: 220px; height: 220px; background: #fafafa; border-radius: 20px; border: 2px solid #eee; text-align: center; display: flex; align-items: center;
+      text{ display: block; flex: 1; font-size: 40px;}
     }
   }
 </style>
