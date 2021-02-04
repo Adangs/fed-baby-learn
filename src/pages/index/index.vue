139f4c39a3a015c45ac8cf1e2c83f3ff652c6cc9
Adang <lundang.liu@feima-inc.com>
2021-02-04 16:13:07 +0800

    增加准备开始动画

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -27,7 +27,7 @@
             <view class="icon">
               <x-icon name="icon-044" color="#555" />
             </view>
-            <view>v1.2.4</view>
+            <view>v1.2.5</view>
           </view>
         </x-button>
       </view>
